735dfc2838ae74e23e6dab5bede97d17f4e6bfca
Payne Whitlock <paynewhitlock@Paynes-MBP-3.attlocal.net>
2024-10-27 22:51:12 -0400

    Updates

--- precache-manifest.dc6185fb56cd8611058f9e3da6b6bf28.js
+++ precache-manifest.dc6185fb56cd8611058f9e3da6b6bf28.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6cc599c4d3c7724bd689b75d99436247",
+    "revision": "81d815a275bdcc18f18345923d924c80",
     "url": "/basketball-junkie-react/index.html"
   },
   {
-    "revision": "6984c75d421a7539d68f",
+    "revision": "235977e29a97829ef668",
     "url": "/basketball-junkie-react/static/css/main.d8ef7cd5.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/basketball-junkie-react/static/js/3.d3741b82.chunk.js"
   },
   {
-    "revision": "6984c75d421a7539d68f",
-    "url": "/basketball-junkie-react/static/js/main.7ba598a6.chunk.js"
+    "revision": "235977e29a97829ef668",
+    "url": "/basketball-junkie-react/static/js/main.070c210b.chunk.js"
   },
   {
     "revision": "35dda61bc4f8f92720e9",
