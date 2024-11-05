dbac194414dee4cae12ff63bc1b3b57c46cdaec8
Payne Whitlock <paynewhitlock@Paynes-MBP-3.lan>
2024-11-05 18:45:58 -0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; // Adjusted path to Navbar
-import Footer from './components/Footer'; // Adjusted path to Footer
-import Home from './pages/Home'; // Adjusted path to Home
-import Players from './pages/Players'; // Adjusted path to Players
-import Teams from './pages/Teams'; // Adjusted path to Teams
-import Analysis from './pages/Analysis'; // Adjusted path to Analysis
-import Contact from './pages/Contact'; // Adjusted path to Contact
-import './styles/App.css'; // Import global styles from the correct path
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Players from './pages/Players';
+import Teams from './pages/Teams';
+import Analysis from './pages/Analysis';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router basename={process.env.NODE_ENV === 'development' ? '' : '/basketball-junkie-react'}>
+    <Router>
       <div className="App">
         <Navbar />
         <Routes>
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Layout from './pages/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import NoPage from './pages/NoPage';
-
-export default function App() {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
